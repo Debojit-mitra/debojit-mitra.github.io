1e2c90820fed940e12b95ff877757d8ce340d6fa
Debojit <Debojit16mitra@gmail.com>
2024-12-24 11:19:05 +0530

    Fix: Update deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/",  // Add this line
+  base: "./",  // Add this line
   plugins: [react()],
 });
 
